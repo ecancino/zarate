52188feb17c63d24d009da0a47b1dd1268ddbd30
Eduardo Cancino <ecancinozarate@deloitte.com>
2017-09-07 23:42:10 -0400

    Add json example

--- app.rb
+++ app.rb
@@ -2,10 +2,16 @@
 
 require 'sinatra'
 require 'slim'
+require 'json'
 
 class Zarate < Sinatra::Base
   get '/' do
     slim :index
+  end
+
+  get '/file.json' do
+    content_type :json
+    { :key1 => 'value1', :key2 => 'value2' }.to_json
   end
 end
 
